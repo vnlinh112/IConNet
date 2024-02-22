907157c6bc2a6e39fce47b30248c82a97bfc3402
Linh PC <vnlinh112@gmail.com>
2024-02-22 20:03:34 +0800

    add new cremad and iemocap experiments

--- IConNet/nn/sequential.py
+++ IConNet/nn/sequential.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 from einops import rearrange, reduce
 from typing import Optional, Literal
+from .mamba import MambaBlock
 
 class Seq2SeqBlocks(nn.Module):
     """
@@ -76,3 +77,41 @@ class Seq2OneBlocks(nn.Module):
             cell_state, '(d l) b h -> b (l d h)', d=self.D, l=self.n_block
         )
         return torch.cat([x, hidden_state, cell_state], dim=-1)
+    
+class MambaSeq2OneBlocks(nn.Module):
+    """B C N -> B C """
+    def __init__(self,
+            n_block: int, 
+            n_input_channel: int, 
+            n_output_channel: int,
+            pooling: Literal[
+                'max', 'min',
+                'mean', 'sum'] = 'mean',
+            kernel_size: int=7,
+            state_expansion_factor: int=2,
+            block_expansion_factor: int=2,
+            ):
+        self.n_block = n_block
+        self.n_input_channel = n_input_channel
+        self.n_output_channel = n_output_channel
+        self.pooling = pooling
+        self.kernel_size = kernel_size
+        self.block_expansion_factor = block_expansion_factor
+        self.state_expansion_factor = state_expansion_factor
+        
+        self.input_projection = nn.Linear(
+            n_input_channel, n_output_channel)
+
+        self.blocks = nn.Sequential(*[MambaBlock( 
+            d_model=self.n_output_channel,
+            d_state=self.state_expansion_factor,  
+            d_conv=self.kernel_size,   
+            expand=self.block_expansion_factor,   
+        ) for _ in range(self.n_block)])
+
+    def forward(self, x):
+        x = self.input_projection(
+            rearrange(x, 'b c n -> b n c'))
+        return reduce(
+            self.blocks(x),
+            'b n c -> b c', self.pooling)
