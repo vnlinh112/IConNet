902163f502ac706cb53867683522d56c5731c27c
linhpc <vnlinh112@gmail.com>
2024-03-02 17:02:41 +0800

    fix sinc

--- IConNet/firconv/firconv.py
+++ IConNet/firconv/firconv.py
@@ -251,7 +251,7 @@ class FirConvLayer(nn.Module):
             torch.sinc(2 * low_time))
         filters_max = reduce(
             self.filters, 'b c l -> b c ()', 'max')
-        self.filters = self.windows * self.filters / filters_max
+        self.filters = self.windows * self.filters #/ filters_max
     
     
     def _firwin_init_filters(self):
