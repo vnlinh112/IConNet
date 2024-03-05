3a8058cc8999db8e4a483a33cd70d455e78c7c5e
Linh PC <vnlinh112@gmail.com>
2024-03-05 17:32:50 +0800

    add hpc sbatch script

--- IConNet/dataset/_crema_d.py
+++ IConNet/dataset/_crema_d.py
@@ -1,3 +1,4 @@
+import torch
 import numpy as np
 from sklearn.model_selection import train_test_split
 
---
+++ sbatch_job.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#SBATCH --job-name=multi_gpu_pytorch
+#SBATCH --time=6:00:00
+#SBATCH --mem=16000
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=16
+#SBATCH --gres=gpu:4g.32gb:2
+#SBATCH --partition=gpu.medium
+
+module add cuda/cudnn/8.4.1 python/3.10.5
+module load anaconda
+
+conda activate audio
+python train.py \
+    '+data_dir=../data/data_preprocessed/' \
+    'model=m13ser' \
+    'train=torch_dry_run_hpc'
