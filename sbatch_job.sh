81f7623ef3af302fafdcb3ae77f3e6ebdfcb95ac
Linh PC <vnlinh112@gmail.com>
2024-03-05 17:46:11 +0800

    update job

--- sbatch_job.sh
+++ sbatch_job.sh
@@ -3,12 +3,11 @@
 #SBATCH --job-name=multi_gpu_pytorch
 #SBATCH --time=6:00:00
 #SBATCH --mem=16000
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=16
-#SBATCH --gres=gpu:4g.32gb:2
+#SBATCH --ntasks=64
+#SBATCH --ntasks-per-node=32
+#SBATCH --cpus-per-task=8
+#SBATCH --gres=gpu:2g.20gb:2
 #SBATCH --partition=gpu.medium
-
-export SRUN_CPUS_PER_TASK=16
 
 module add cuda/cudnn/8.4.1 python/3.10.5
 module load anaconda
